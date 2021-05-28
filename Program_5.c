eac81c9c33dfac5592754c0c7b953e8f2a9ac8d0
ranjanaau <>
2021-05-28 05:27:02 +0000

    Largest and smallest

--- Program_5.c
+++ Program_5.c
@@ -5,13 +5,34 @@
 
 int main()
 {
-    int arr[10], n, i;
-    scanf("%d", &n);
-
-    for(i = 0; i < n; i++)
-        scanf("%d", &arr[i]);
+    int arr[10], n, i, large,small;
 
     //Write your code here
+    printf("Enter the size of array");
+    scanf("%d",&n);
+    printf("Enter the elements in array\n");
+    for(i=0;i<n;i++)
+    {
+      scanf("%d",&arr[i]);
+    }
+    large=small=arr[0];
+    for(i=1;i<n;i++)
+    {
+      if(large < arr[i])
+      {
+      large=arr[i];
+      }
+      else if(small>arr[i])
+      small=arr[i];
+    }
+  
+      printf("largest element is %d",large);
+      printf("smallest element is %d",small);
+  
+
+  
+    
+
   
     return 0;
 }
